59ee2022b4a69eb5c91928caf3410e09f0fdc088
Paulo Andreola <pauloandreola@gmail.com>
2023-02-22 21:16:59 -0300

    feat: create user table migration

---
+++ src/database/migrations/1677029455216-CreateUserTable.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateUserTable1677029455216 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
+
