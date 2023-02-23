d96be93dd24a86404e8fc9fbaf6acda663cdb820
Paulo Andreola <pauloandreola@gmail.com>
2023-02-22 21:16:02 -0300

    refactor: clean code and update entities and migrations destination

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,20 @@
 import 'dotenv/config';
 import 'reflect-metadata';
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
 
 const port = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: port,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  synchronize: true,
-  logging: true,
-  entities: ["src/app/models/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
-//   cli: {
-//     "entitiesDir": "src/app/models",    
-//     "migrationsDir": "src/database/migrations"
-//   }
+  entities: [`${__dirname}/**/models/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+//   entities: ['src/app/models/*.ts'],
+//   migrations: ['src/database/migrations/*.ts'],
 })
 
 AppDataSource.initialize()
